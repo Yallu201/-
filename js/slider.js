a7a0f0ec9886dbeb0fe94820ca802772cd0e16f1
LEE <dlruddlf07@gmail.com>
2019-06-18 07:20:15 +0900

    page1 slide color

--- js/slider.js
+++ js/slider.js
@@ -6,33 +6,63 @@
 		window.addEventListener('scroll',function(e){
 			if(scrollBefore < window.scrollY){scrollDown = true;}
 			else{scrollDown = false;}
-			
-			if(scrollDown){
-				if(scrollY < 1280){
-					header.css('color','#fff')
-					square.css('background','#fff');
-				}else if(scrollY < 2360){
-					header.css('color','#000')
-					square.css('background','#000');
-				}else if(scrollY <= 3440){
-					header.css('color','#fff')
-					square.css('background','#fff');
-				}
-			}else{
-				if(scrollY == 0){
-					header.css('color','#000')
-					square.css('background','#000');
-				}else if(scrollY < 1080){
-					header.css('color','#fff')
-					square.css('background','#fff');
-				}else if(scrollY < 2160){
-					header.css('color','#000')
-					square.css('background','#000');
-				}else if(scrollY <= 3240){
-					header.css('color','#fff')
-					square.css('background','#fff');
-				}
+			/* page 1 */
+			if(scrollY == 0){
+				header.css('color','#000').fadeIn(1250);
+				square.css('background','#000');
+			}else if(scrollY > 0 && scrollY < 880){
+				header.css('color','#fff').fadeOut('fast');
+				square.css('background','#fff');
 			}
+			/* page 2 */
+			else if(scrollY < 1196 && scrollY >= 880){
+				header.css('color','#fff').fadeIn(1250);
+				square.css('background','#fff');
+			}else if(scrollY >= 1196 && scrollY < 2091){
+				header.css('color','#000').fadeOut('fast');
+				square.css('background','#000');
+			}
+			/* page 3 */
+			else if(scrollY < 2255 && scrollY >= 2091){
+				header.css('color','#000').fadeIn(1250);
+				square.css('background','#000');
+			}else if(scrollY < 3040 && scrollY >= 2255){
+				header.css('color','#fff').fadeOut('fast');
+				square.css('background','#fff');
+			}
+			/* page 4 */
+			else if(scrollY >= 3040){
+				header.css('color','#fff').fadeIn(1250);
+				square.css('background','#fff');
+			}
+			
+			/* 색깔만 변경 */
+//			if(scrollDown){
+//				if(scrollY < 1280){
+//					header.css('color','#fff')
+//					square.css('background','#fff');
+//				}else if(scrollY < 2360){
+//					header.css('color','#000')
+//					square.css('background','#000');
+//				}else if(scrollY <= 3440){
+//					header.css('color','#fff')
+//					square.css('background','#fff');
+//				}
+//			}else{
+//				if(scrollY == 0){
+//					header.css('color','#000')
+//					square.css('background','#000');
+//				}else if(scrollY < 1080){
+//					header.css('color','#fff')
+//					square.css('background','#fff');
+//				}else if(scrollY < 2160){
+//					header.css('color','#000')
+//					square.css('background','#000');
+//				}else if(scrollY <= 3240){
+//					header.css('color','#fff')
+//					square.css('background','#fff');
+//				}
+//			}
 			scrollNow = window.scrollY;
 		});
 		/* header float effect*/
